0d658885d0e633e4b760f5a72522932053f38574
4nt04n <4nt04n@gmail.com>
2021-03-21 12:50:24 -0300

    Creando Form,Header y agregando Styled

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { Fragment,useState } from "react";
+import Header from "./Components/Header";
+import Form from "./Components/Form";
+import "./index.css";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <Header titulo="Clima React App" />
+      <div className="contenedor-form">
+        <div className="container ">
+          <div className="row">
+            <div className="col m6 s12">
+              <Form />
+            </div>
+            <div className="col m6 s12">2</div>
+          </div>
+        </div>
+      </div>
+    </Fragment>
   );
 }
 
---
+++ src/Components/Form.js
@@ -0,0 +1,70 @@
+import React, { useState } from "react";
+
+const Form = () => {
+  //State form
+  const [search, setSearch] = useState({
+    ciudad: "",
+    pais: "",
+  });
+  //State Error
+  const [error,setError]=useState(false);
+
+//destructuring search
+const {ciudad,pais}=search;
+
+//save function
+const handleChange = e =>{
+  //actulizar el state 
+  setSearch({
+    ...search,[e.target.name]:e.target.value 
+  });
+}
+const handleSubmit = e =>{
+  e.preventDefault();
+  if (ciudad.trim() ==='' || pais.trim()==='')
+  {
+    setError(true)
+    return;
+  }
+  setError(false);
+}
+  return (
+    <form
+      onSubmit={handleSubmit}
+    >
+      {error?<p className="red darken-4 error">Todos los campos son obligatorios</p>:null}
+      <div className="input-field col s12">
+        <input type="text" 
+        name="ciudad" 
+        id="ciudad" 
+        value={ciudad}
+        onChange = {handleChange}
+        />
+        <label htmlFor="ciudad"> Ciudad: </label>
+      </div>
+      <div className="input-field col s12">
+        <select name="pais" id="pais" onChange = {handleChange}>
+          <option value="">-- Seleccione un pais --</option>
+          <option value="US">Estados Unidos</option>
+          <option value="MX">México</option>
+          <option value="AR">Argentina</option>
+          <option value="CO">Colombia</option>
+          <option value="CR">Costa Rica</option>
+          <option value="ES">España</option>
+          <option value="PE">Perú</option>
+        </select>
+        <label htmlFor="pais">Pais: </label>
+      </div>
+
+      <div className="input-field col s12">
+          <button
+          type="submit"
+          className="waves-effect waves-light btn-large btn-block yellow accent-4 col s12"
+         >  Buscar Clima   
+          </button>
+      </div>
+    </form>
+  );
+};
+
+export default Form;
